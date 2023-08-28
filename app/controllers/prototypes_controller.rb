e21dc6dac19f5c37117b5970e471d4ce0f152cae
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2023-08-28 16:23:01 +0900

    必要ないパラメーターを削除

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -45,6 +45,6 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id, prototype_id: params[:id] )
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id )
   end
 end
